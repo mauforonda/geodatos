4219fb40dc346387541d8c6b0cfd310a25eedd2b
mauforonda <mauforonda@users.noreply.github.com>
2024-07-26 13:56:56 +0000

    deploy: 2a4777447b10139c3e132fc924528cca3c204f29

---
+++ _import/components/cargar.49299a24.js
@@ -0,0 +1,13 @@
+import {mostrarCapa} from "./mostrar.7d0cf371.js"
+import {csv, json} from "../../_npm/d3@7.9.0/_esm.js"
+
+export async function cargarCapas() {
+    const baseUrl = "https://raw.githubusercontent.com/mauforonda/geodatos/master"
+    async function cargarDirectorio() {
+        const d = await json(`${baseUrl}/directorio.json`)
+        return Object.fromEntries(d.map((i) => [i.nombre, i]))
+    }
+    const directorio = await cargarDirectorio()
+    const c = await csv(`${baseUrl}/capas.csv`)
+    return c.map(i => { return {...i, presentacion:mostrarCapa(i, directorio)} }).reverse()
+}
